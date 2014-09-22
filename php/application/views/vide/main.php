bd87e3abec62b9147f463dfc48cc3363702c61e4
SSS <song_meng_wee@hotmail.com>
2014-09-22 15:16:50 +0800

    V0.0

--- php/application/views/vide/main.php
+++ php/application/views/vide/main.php
@@ -4,7 +4,7 @@
 		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
 		<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
 		
-		<script src="<?php echo base_url(). "js/submit.js" ?>"></script>
+		<script src="<?php echo base_url(). "js/newUser.js" ?>"></script>
 	
 		<title>Visual IDE Group 16</title>
 		
