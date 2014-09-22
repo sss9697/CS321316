f761b43d934fe1c9cdf6550d23699dd0cbfbfef1
SSS <song_meng_wee@hotmail.com>
2014-09-22 15:55:58 +0800

    V0.11
    Signin + Create user UI

--- php/application/views/vide/main.php
+++ php/application/views/vide/main.php
@@ -5,6 +5,7 @@
 		<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
 		
 		<script src="<?php echo base_url(). "js/newUser.js" ?>"></script>
+		<script src="<?php echo base_url(). "js/signin.js" ?>"></script>
 	
 		<title>Visual IDE Group 16</title>
 		
@@ -14,11 +15,26 @@
 			<p class="validateTips">All form fields are required.</p>
 			<form>
 				<fieldset>
-					<label for="name">Name</label>
+					<label for="name">Name:   </label>
 					<input type="text" name="name" id="name" value="" class="text ui-widget-content ui-corner-all"><br>
-					<label for="email">Email</label>
+					<label for="email">Email:  </label>
 					<input type="text" name="email" id="email" value="" class="text ui-widget-content ui-corner-all"><br>
-					<label for="password">Password</label>
+					<label for="password">Password:</label>
+					<input type="password" name="password" id="password" value="" class="text ui-widget-content ui-corner-all"><br>
+					<!-- Allow form submission with keyboard without duplicating the dialog button -->
+					<input type="submit" tabindex="-1" style="position:absolute; top:-1000px">
+				</fieldset>
+			</form>
+		</div>
+		
+		<div id="signin-form" title="Sign in">
+			<p class="validateTips">All form fields are required.</p>
+			<form>
+				<fieldset>
+					<label for="email">Email:  </label>
+					<input type="text" name="email" id="email" value="" class="text ui-widget-content ui-corner-all"><br>
+					<font color="red">Google account will use google login</font> 
+					<label for="password">Password:</label>
 					<input type="password" name="password" id="password" value="" class="text ui-widget-content ui-corner-all"><br>
 					<!-- Allow form submission with keyboard without duplicating the dialog button -->
 					<input type="submit" tabindex="-1" style="position:absolute; top:-1000px">
--- php/js/signin.js
+++ php/js/signin.js
@@ -2,10 +2,9 @@ $(function()
 {
 	var dialog, form,
 	emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-	name = $( "#name" ),
 	email = $( "#email" ),
 	password = $( "#password" ),
-	allFields = $( [] ).add( name ).add( email ).add( password ),
+	allFields = $( [] ).add( email ).add( password ),
 	tips = $( ".validateTips" );
 
 	function updateTips( t ) 
@@ -48,29 +47,27 @@ $(function()
 		}
 	}
 	
-	function addUser() 
+	function validateUser() 
 	{
 		var valid = true;
 		allFields.removeClass( "ui-state-error" );
-		valid = valid && checkLength( name, "username", 3, 16 );
 		valid = valid && checkLength( email, "email", 6, 80 );
 		valid = valid && checkLength( password, "password", 5, 16 );
-		valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
 		valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
 		valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
 		if ( valid ) 
 		{
-			//Need insert to DB here
+			//Need to validate with DB here
 			//hashing password?
 			
-			window.alert("Welcome " + name);
+			window.alert(name + " Signed in");
 			
 			dialog.dialog( "close" );
 		}
 		return valid;
 	}
 	
-	dialog = $( "#newuser-form" ).dialog(
+	dialog = $( "#signin-form" ).dialog(
 	{
 		autoOpen: false,
 		height: 400,
@@ -78,10 +75,10 @@ $(function()
 		modal: true,
 		buttons: 
 		{
-			"Create an account": addUser,
+			"Sign in": validateUser,
 			Cancel: function() 
 			{
-			dialog.dialog( "close" );
+				dialog.dialog( "close" );
 			}
 		},
 		close: function() 
@@ -96,9 +93,9 @@ $(function()
 		event.preventDefault();
 		addUser();
 	});
-	$( "#create-user" ).button().on( "click", function() 
+	$( "#validate-user" ).button().on( "click", function() 
 	{
 	
-	dialog.dialog( "open" );
+		dialog.dialog( "open" );
 	});
 });
