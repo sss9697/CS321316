f8a4f0915ebc709b4218e398c9b6397b4663f271
SSS <song_meng_wee@hotmail.com>
2014-09-22 13:47:32 +0800

    CRA

--- php/application/views/templates/header.php
+++ php/application/views/templates/header.php
@@ -2,8 +2,8 @@
 <head>
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
 	 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
-
+	<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
+	<script src="<?php echo base_url(). "js/submit.js" ?>"></script>
 	<title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
 	
 
