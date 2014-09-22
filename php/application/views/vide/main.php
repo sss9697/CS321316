ed5b0ba76646a2991ab8b3386dcd80624dfd92e9
SSS <song_meng_wee@hotmail.com>
2014-09-22 15:10:29 +0800

    V0.0
    Base page setup

--- php/application/views/vide/main.php
+++ php/application/views/vide/main.php
@@ -6,8 +6,47 @@
 		
 		<script src="<?php echo base_url(). "js/submit.js" ?>"></script>
 	
-		<title>Visual IDE Main and only page</title>
+		<title>Visual IDE Group 16</title>
 		
-		Like seriously, what am i going to do from here on
+		
+		
+		<div id="dialog-form" title="Create new user">
+			<p class="validateTips">All form fields are required.</p>
+			<form>
+				<fieldset>
+					<label for="name">Name</label>
+					<input type="text" name="name" id="name" value="Jane Smith" class="text ui-widget-content ui-corner-all">
+					<label for="email">Email</label>
+					<input type="text" name="email" id="email" value="jane@smith.com" class="text ui-widget-content ui-corner-all">
+					<label for="password">Password</label>
+					<input type="password" name="password" id="password" value="xxxxxxx" class="text ui-widget-content ui-corner-all">
+					<!-- Allow form submission with keyboard without duplicating the dialog button -->
+					<input type="submit" tabindex="-1" style="position:absolute; top:-1000px">
+				</fieldset>
+			</form>
+		</div>
+		
+		<div id="users-contain" class="ui-widget">
+			<h1>Existing Users:</h1>
+			<table id="users" class="ui-widget ui-widget-content">
+				<thead>
+					<tr class="ui-widget-header ">
+						<th>Name</th>
+						<th>Email</th>
+						<th>Password</th>
+					</tr>
+				</thead>
+				
+				<tbody>
+					<tr>
+						<td>John Doe</td>
+						<td>john.doe@example.com</td>
+						<td>johndoe1</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		
+		<button id="create-user">New user</button> <button id="create-user">Sign in</button> 
 	</head>
 </html>
