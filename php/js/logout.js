9f2418a171abcb626f4f44a93b146dc233e6000a
SSS <song_meng_wee@hotmail.com>
2014-09-25 03:19:51 +0800

    V0.15
    Im really retarded =(

--- php/js/logout.js
+++ php/js/logout.js
@@ -1,6 +1,6 @@
 	function logoutAction()
 	{
-		document.getElementById('logout-btn').style.visibility="none";
+		document.getElementById('logout-btn').style.visibility="hidden";
 		document.getElementById('create-user').style.visibility="visible";
 		document.getElementById('validate-user').style.visibility="visible";
 		document.getElementById('validate-google').style.visibility="visible";
