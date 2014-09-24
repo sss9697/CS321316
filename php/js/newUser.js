6e3565618c06cdbf9f034fd2c71147b2727e5a25
SSS <song_meng_wee@hotmail.com>
2014-09-24 15:27:25 +0800

    V0.13

--- php/js/newUser.js
+++ php/js/newUser.js
@@ -1,7 +1,7 @@
 $(function() 
 {
 	var dialog, form,
-	var base_url = '<?php echo site_url();
+	var base_url = '<?php echo site_url()';
 	emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
 	name = $( "#name" ),
 	email = $( "#email" ),
