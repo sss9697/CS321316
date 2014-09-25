b4296caa9e7f0d8a3cb50210ab79ef077d7aa9e9
SSS <song_meng_wee@hotmail.com>
2014-09-25 17:45:13 +0800

    V0.23

--- php/js/editordivui.js
+++ php/js/editordivui.js
@@ -32,6 +32,7 @@ $(function()
 		if(e.which != 8 && e.which != 0 && e.which != 45 && (e.which < 48 || e.which > 57))
 		{
 			$("#errmsg").html("Digits Only").show().fadeOut("slow");
+			return false;
 		}
 	});
 });
