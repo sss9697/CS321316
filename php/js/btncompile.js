d33bed2b463ca1b03d2a81bcc44cf10ddd975a74
SSS <song_meng_wee@hotmail.com>
2014-09-28 03:51:01 +0800

    V0.25

--- php/js/btncompile.js
+++ php/js/btncompile.js
@@ -7,7 +7,7 @@ function compilef()
 	
 	var numberofcommand = $("#droppable").children().length;
 	var test = $("#droppable").children().children().children().get(0).tagName;
-	var test2 = $("#droppable").children().get(0).attr("id");
+	var test2 = $("#droppable > li").attr("id");
 	
 	alert("First command is: "+test2 + " " + test+ " times");
 }
