32aeed7206c85b3622b838f065bf7a3a84795e9b
SSS <song_meng_wee@hotmail.com>
2014-09-28 03:15:01 +0800

    V0.25

--- php/js/btncompile.js
+++ php/js/btncompile.js
@@ -2,6 +2,6 @@ function compilef()
 {
 	var numberofcommand = $("#droppable").children().length;
 	var test = $("#droppable").children().get(0).tagName;
-	var test2 = test.children.get(0).tagName;
+	var test2 = test.children().get(0).tagName;
 	alert(test + " " + test2);
 }
