42a635ca8c8d39e387a9299b462dc8e67ee9558d
SSS <song_meng_wee@hotmail.com>
2014-09-28 04:01:21 +0800

    V0.25

--- php/js/btncompile.js
+++ php/js/btncompile.js
@@ -2,11 +2,14 @@ function compilef()
 {
 	//this following functions are just for testing
 	//this function is suppose to call compilecommand which will compile the command to a string or array and return
-	//then it will call the player window to execute the commands
+	//then it will call the expand command then the player window to execute the commands
 	//similarly, the save button should call compilecommand then call its DB function to post.
 	
+	//after compile command, we will have an expand command function
+	//expand command will remove all loops and expand them into multiple instance of instructions
+	//Whether it should change up5 to up1 up1 up1 up1 up1 is still to be determined
 	var numberofcommand = $("#droppable").children().length;
-	var test = $("#droppable > input").attr("value");
+	var test = $("#droppable > input").val();
 	var test2 = $("#droppable > li").attr("id");
 	
 	alert("First command is: "+test2 + " " + test+ " times");
