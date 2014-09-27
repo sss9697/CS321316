67aea41fea3c34d458cc560a45765066929bb367
SSS <song_meng_wee@hotmail.com>
2014-09-28 03:23:20 +0800

    V0.25

--- php/js/btncompile.js
+++ php/js/btncompile.js
@@ -1,7 +1,12 @@
 function compilef()
 {
+	//this following functions are just for testing
+	//this function is suppose to call compilecommand which will compile the command to a string or array and return
+	//then it will call the player window to execute the commands
+	//similarly, the save button should call compilecommand then call its DB function to post.
+	
 	var numberofcommand = $("#droppable").children().length;
 	var test = $("#droppable").children().get(0).tagName;
-	var test2 = test.children().get(0).tagName;
+	var test2 = $("#droppable").children().children().get(0).tagName;
 	alert(test + " " + test2);
 }
