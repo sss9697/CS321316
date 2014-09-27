9b13f3541e966757643c38d1f120ffbdfae19869
SSS <song_meng_wee@hotmail.com>
2014-09-28 03:32:48 +0800

    V0.25

--- php/js/btncompile.js
+++ php/js/btncompile.js
@@ -6,7 +6,7 @@ function compilef()
 	//similarly, the save button should call compilecommand then call its DB function to post.
 	
 	var numberofcommand = $("#droppable").children().length;
-	var test = $("#droppable").children().children().children().get(0).val();
+	var test = $("#droppable").children().children().children().get(0).tagName;
 	var test2 = $("#droppable").children().children().get(0).text();
 	
 	alert("First command is: "+test2 + " " + test+ " times");
