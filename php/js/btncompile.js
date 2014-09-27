af0cf175a8ab425337a33eb301d762c647c01cbd
SSS <song_meng_wee@hotmail.com>
2014-09-28 03:12:35 +0800

    V0.25
    testing some functions

--- php/js/btncompile.js
+++ php/js/btncompile.js
@@ -1,5 +1,7 @@
 function compilef()
 {
 	var numberofcommand = $("#droppable").children().length;
-	alert(numberofcommand);
+	var test = $("#droppable").children().get(0).tagName;
+	var test2 = test.children.get(0).tagName;
+	alert(test + " " + test2);
 }
