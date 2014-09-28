a5d11fac8099308f977fdfb536490a4021c781b6
SSS <song_meng_wee@hotmail.com>
2014-09-29 01:49:44 +0800

    V0.27

--- php/js/btncompile.js
+++ php/js/btncompile.js
@@ -1,7 +1,6 @@
+//change user input command into a set of string command, used for DB as well as processing
 function compileCommand()
 {
-	//at this point of time, even if the moveset is invalid, we just compile it as this function may be used to save to DB.
-	//the validity check will be done at another part
 	var compileString = "";
 	$('#droppable').children().each(function () 
 	{
@@ -40,34 +39,36 @@ function compileCommand()
 			default: compileString += "U";
 		}
 		
-		compileString += numberTimes + " ";
-		//console.log("command is: "+commandType + " " + numberTimes+ " times");
+		compileString += numberTimes + ",";
 	});
-	console.log("CC: " + compileString);
 	return compileString;
 }
 
-function compilef()
+//convert string command into array (Might be optional)
+function convertCommand(input)
 {
-	//this following functions are just for testing
-	//this function is suppose to call compilecommand which will compile the command to a string or array and return
-	//then it will call the expand command then the player window to execute the commands
-	//similarly, the save button should call compilecommand then call its DB function to post.
+	var jsonTemp = "["+input+"]";
+	var output = JSON.parse(jsonTemp);
 	
-	//after compile command, we will have an expand command function
-	//expand command will remove all loops and expand them into multiple instance of instructions
-	//Whether it should change up5 to up1 up1 up1 up1 up1 is still to be determined
-	//expand should also change left-5 to right5 or down-7 to up7
-	var numberofcommand = $("#droppable").children().length;
-	var test = $("#droppable > li > div > input").val();
-	var test2 = $("#droppable > li").attr("id");
-	
-	var test3 = $("#droppable > li:eq(1) > div > input").val();
-	var test4 = $("#droppable > li").eq(1).attr("id");
-	
-	//alert("First command is: "+test2 + " " + test+ " times");
-	//alert("Second command is: "+test4 + " " + test3+ " times");
-	
+	console.log(output[0]);
+	console.log(output[1]);
+}
+
+//validate user input command, the loops
+function validateCommand()
+{
+}
+
+//remove the loop and expand out the commands
+function expandCommand()
+{
+}
+
+
+function compilef()
+{	
 	var command = compileCommand();
 	console.log(command);
+	
+	convertCommand(command);
 }
