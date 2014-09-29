9d5e1c0c53deb9429ba40300500b4c023cbf945a
SSS <song_meng_wee@hotmail.com>
2014-09-29 18:42:56 +0800

    V0.33

--- php/js/loadController.js
+++ php/js/loadController.js
@@ -1,5 +1,11 @@
 function loadf(moves)
 {
-	var movearray = moves.split(" ");
+	var movearray = moves.trim().split(" ");
+	
+	$( "#editorspace ul" ).find( ".placeholder" ).remove();
+	for( i = 0 ; i < movearray.length ; i++)
+	{
+		
+	}
 	alert(movearray);
 }
