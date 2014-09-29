069a186b986c314dbf807387e146482ec8d7828e
SSS <song_meng_wee@hotmail.com>
2014-09-29 17:48:08 +0800

    V0.32
    Load list done
    Now need to reload list after a new save is entered

--- php/js/btnSaveController.js
+++ php/js/btnSaveController.js
@@ -18,6 +18,28 @@ function convertCommand(input)
 	return returnString;
 }
 
+function updateList()
+{
+	$.ajax(
+	{
+		url : "https://php-cs321316.rhcloud.com/index.php/vide/get_move",
+		type : 'POST',
+		datatype : "json",
+		data : {"PID":uID},
+		success : function(moves)
+		{
+			for( v = 0 ; v < moves.length ; v ++)
+			{
+				$("#loadList").append($('<option>').text(moves[v].Name).attr('value', moves[v].Moveset));
+			}
+		},
+		error : function(jqXHR, textStatus, errorThrown)
+		{
+			alert(textStatus + " " + errorThrown + " " + jqXHR);
+		}
+	});
+}
+
 function saveMove()
 {
 	var commands = compileCommand();
@@ -31,6 +53,7 @@ function saveMove()
 		success : function()
 		{
 			alert($("#savename").val()+ " Saved");
+			updateList();
 		},
 		error : function(jqXHR, textStatus, errorThrown)
 		{
