0b58e6ba058a6c3e26eb1d4ea2baeeb0a2a1c67f
SSS <song_meng_wee@hotmail.com>
2014-09-30 02:52:33 +0800

    V0.5

--- php/js/btnToggleGridController.js
+++ php/js/btnToggleGridController.js
@@ -3,12 +3,12 @@ function toggleGridf(state)
 console.log(state.value);
 	if(state.value == "show")
 	{
-		$('#divID').css("background-image", "url('../img/smallgrid.png')"); 
+		$('#playerdiv').css("background-image", "url('../img/smallgrid.png')"); 
 		state.value = "hide";
 	}
 	else
 	{
-		$('#divID').css("background-image", ""); 
+		$('#playerdiv').css("background-image", ""); 
 		state.value = "show";
 	}
 }
