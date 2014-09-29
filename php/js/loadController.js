f9bf157cf054a3590bf4941edf006c9382c6aa1d
SSS <song_meng_wee@hotmail.com>
2014-09-29 18:54:33 +0800

    V0.33

--- php/js/loadController.js
+++ php/js/loadController.js
@@ -5,7 +5,28 @@ function loadf(moves)
 	$( "#editorspace ul" ).empty();
 	for( i = 0 ; i < movearray.length ; i++)
 	{
-		
+		switch(movearray[i].charat(0))
+		{
+			case 'U': $( "#editorspace ul" ).append(
+						$("<li id=\"moveup\" class=\"command\">	<div id=\"movediv\">up: <input type=\"number\" name=\"edittxt\" id=\"edittxt\" value=\"1\" max=\"999\" min=\"-999\" /></div></li>");
+				break;
+			case 'D': 
+				break;
+			case 'L': 
+				break;
+			case 'R': 
+				break;
+			case 'O': 
+				break;
+			case 'C': 
+				break;
+			case 'A': 
+				break;
+			case 'B': 
+				break;
+			case 'T': 
+				break;
+		}
 	}
 	alert(movearray);
 }
