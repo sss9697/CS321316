0dde1052a8248ba8451f790b51a83c4f9bbc0496
SSS <song_meng_wee@hotmail.com>
2014-09-30 16:06:13 +0800

    V0.51

--- php/js/btnToggleCharController.js
+++ php/js/btnToggleCharController.js
@@ -1,15 +1,15 @@
 function toggleCharf()
 {
 	var cha = $('#sprite');
-	console.log(cha.attr("value"));
+
 	if(cha.attr("value") == "show")
 	{
 		cha.fadeIn(500);
-		cha.attr("value") = "hide";
+		cha.attr("value", "hide");
 	}
 	else
 	{
 		cha.fadeOut(500);
-		cha.attr("value") = "show";
+		cha.attr("value", "show");
 	}
 }
