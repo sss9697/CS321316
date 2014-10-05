d5d18cc11a60f21e43d5f09d4973153e5bdf555b
Larry Lee <meandmyipod@gmail.com>
2014-10-05 18:42:25 +0800

    Revert

--- php/application/views/vide/main.php
+++ php/application/views/vide/main.php
@@ -204,43 +204,4 @@
 			</div>		
 		</div>
 	</head>
-
-	<body>
-
-	<span id="signinButton">
-	  <span
-	    class="g-signin"
-	    data-callback="signinCallback"
-	    data-clientid="177325121472-1rgfp4ninj9t32d6pb1v68vm8cb7avie.apps.googleusercontent.com"
-	    data-cookiepolicy="single_host_origin"
-	    data-requestvisibleactions="http://schema.org/AddAction"
-	    data-scope="https://www.googleapis.com/auth/plus.login">
-	  </span>
-	</span>
-
-	<script type="text/javascript">
-      (function() {
-       var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-       po.src = 'https://apis.google.com/js/client:plusone.js';
-       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-     })();
-
-     function signinCallback(authResult) {
-  if (authResult['status']['signed_in']) {
-    // Update the app to reflect a signed in user
-    // Hide the sign-in button now that the user is authorized, for example:
-    document.getElementById('signinButton').setAttribute('style', 'display: none');
-  } else {
-    // Update the app to reflect a signed out user
-    // Possible error values:
-    //   "user_signed_out" - User is signed-out
-    //   "access_denied" - User denied access to your app
-    //   "immediate_failed" - Could not automatically log in the user
-    console.log('Sign-in state: ' + authResult['error']);
-  }
-}
-
-    </script>
-
-	</body>
 </html>
